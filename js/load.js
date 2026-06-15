7b029ebf8c9e27ee506140bacc7e404445cbc7a2
東雲 <itousinnsuke@gmail.com>
2026-06-15 15:14:29 +0900

    修改动画显示逻辑

--- js/load.js
+++ js/load.js
@@ -1,24 +1,33 @@
 const progressBar = document.querySelector(".progress");
 const loader = document.querySelector(".loader");
 
-let progress = 0;
-
-const timer = setInterval(() => {
-  progress ++;
-
-  progressBar.style.width = progress + "%";
-
-  if (progress >= 100) {
-    clearInterval(timer);
-
-    // 淡出 loading
-    loader.style.opacity = "0";
-
-    setTimeout(() => {
-      loader.style.display = "none";
-
-      // 👉 显示主页
-      document.body.classList.add("loaded");
-    }, 500);
+if (sessionStorage.getItem("visited")){
+  document.body.classList.add("loaded");
+  if (loader){
+    loader.style.display = "none";
   }
-}, 30);
+}
+else {
+  sessionStorage.setItem("visited","true");
+  let progress = 0;
+
+  const timer = setInterval(() => {
+    progress ++;
+
+    progressBar.style.width = progress + "%";
+
+    if (progress >= 100) {
+      clearInterval(timer);
+
+      // 淡出 loading
+      loader.style.opacity = "0";
+
+      setTimeout(() => {
+        loader.style.display = "none";
+
+        // 👉 显示主页
+        document.body.classList.add("loaded");
+      }, 500);
+    }
+  }, 30);
+}
